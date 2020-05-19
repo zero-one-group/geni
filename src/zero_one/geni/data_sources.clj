ea79a4a53b240e586ccd24df7a630c5afa876122
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-05-19 21:31:58 +0700

    Added options + modes + partitions for reader/writer

--- src/zero_one/geni/data_sources.clj
+++ src/zero_one/geni/data_sources.clj
@@ -1,53 +1,59 @@
 (ns zero-one.geni.data-sources)
 
-(defn read-parquet! [spark-session path]
-  (.. spark-session
-      read
-      (parquet path)))
+;; TODO:
+;; Writer: format, option, saveMode, partitionBy, bucketBy, sortBy
+;; File Types: text
 
-(defn write-parquet! [dataframe path]
-  (.. dataframe
-      write
-      (mode "overwrite")
-      (parquet path)))
+(defn read-data! [format spark-session path options]
+  (let [unconfigured-reader (.. spark-session read (format format))
+        configured-reader   (reduce
+                              (fn [r [k v]] (.option r k v))
+                              unconfigured-reader
+                              options)]
+    (.load configured-reader path)))
 
-(defn write-csv! [dataframe path]
-  (.. dataframe
-      write
-      (format "com.databricks.spark.csv")
-      (option "header" "true")
-      (mode "overwrite")
-      (save path)))
+(defn read-parquet!
+  ([spark-session path] (read-data! "parquet" spark-session path {}))
+  ([spark-session path options] (read-data! "parquet" spark-session path options)))
 
-(defn read-csv! [spark-session path]
-  (.. spark-session
-      read
-      (format "csv")
-      (option "header" "true")
-      (load path)))
+(defn read-csv!
+  ([spark-session path] (read-data! "csv" spark-session path {"header" "true"}))
+  ([spark-session path options] (read-data! "csv" spark-session path options)))
 
-(defn read-libsvm! [spark-session path]
-  (.. spark-session
-      read
-      (format "libsvm")
-      (load path)))
+(defn read-libsvm!
+  ([spark-session path] (read-data! "libsvm" spark-session path {}))
+  ([spark-session path options] (read-data! "libsvm" spark-session path options)))
 
-(defn write-libsvm! [dataframe path]
-  (.. dataframe
-      write
-      (format "libsvm")
-      (mode "overwrite")
-      (save path)))
+(defn read-json!
+  ([spark-session path] (read-data! "json" spark-session path {}))
+  ([spark-session path options] (read-data! "json" spark-session path options)))
 
-(defn read-json!  [spark-session path]
-  (.. spark-session
-      read
-      (format "json")
-      (load path)))
+(defn write-data! [format dataframe path options]
+  (let [unconfigured-writer (-> dataframe
+                                (.write)
+                                (.format format)
+                                (cond-> (:mode options) (.mode (:mode options)))
+                                (cond-> (:bucket-by options) (.bucketBy (:bucket-by options)))
+                                (cond-> (:sort-by options) (.sortBy (:sort-by options)))
+                                (cond-> (:partition-by options) (.partitionBy (:partition-by options))))
+        configured-writer   (reduce
+                              (fn [w [k v]] (.option w k v))
+                              unconfigured-writer
+                              (dissoc options :mode :bucket-by :sort-by :partition-by))]
+    (.save configured-writer path)))
 
-(defn write-json! [dataframe path]
-  (.. dataframe
-      write
-      (format "json")
-      (mode "overwrite")
-      (save path)))
+(defn write-parquet!
+  ([dataframe path] (write-data! "parquet" dataframe path {}))
+  ([dataframe path options] (write-data! "parquet" dataframe path options)))
+
+(defn write-csv!
+  ([dataframe path] (write-data! "csv" dataframe path {"header" "true"}))
+  ([dataframe path options] (write-data! "csv" dataframe path (merge options {"header" "true"}))))
+
+(defn write-libsvm!
+  ([dataframe path] (write-data! "libsvm" dataframe path {}))
+  ([dataframe path options] (write-data! "libsvm" dataframe path options)))
+
+(defn write-json!
+  ([dataframe path] (write-data! "json" dataframe path {}))
+  ([dataframe path options] (write-data! "json" dataframe path options)))
--- test/zero_one/geni/data_sources_test.clj
+++ test/zero_one/geni/data_sources_test.clj
@@ -1,42 +1,61 @@
 (ns zero-one.geni.data-sources-test
   (:require
-    [midje.sweet :refer [fact =>]]
+    [midje.sweet :refer [fact => throws]]
     [zero-one.geni.core :as g]
     [zero-one.geni.test-resources :refer [create-temp-file!
                                           melbourne-df
                                           libsvm-df
-                                          spark]]))
+                                          spark]])
+  (:import
+    (org.apache.spark.sql AnalysisException)))
 
 (def write-df
   (-> melbourne-df (g/select "Method" "Type") (g/limit 5)))
 
+(fact "Writer defaults to error"
+  (let [temp-file (.toString (create-temp-file! ".csv"))]
+    (g/write-csv! write-df temp-file {:mode "overwrite"})
+    (g/write-csv! write-df temp-file)) => (throws AnalysisException))
+
+(fact "Can read with options"
+  (let [read-df (g/read-parquet!
+                  spark
+                  "test/resources/melbourne_housing_snapshot.parquet"
+                  {"mergeSchema" "true"})]
+    (g/count read-df) => 13580)
+  (let [temp-file (.toString (create-temp-file! ".csv"))
+        read-df  (do (g/write-csv! write-df temp-file {:mode "overwrite"})
+                     (g/read-csv! spark temp-file {"header" "false"}))]
+    (set (g/column-names read-df)) => #(not= % #{"Method" "Type"}))
+  (let [temp-file (.toString (create-temp-file! ".libsvm"))
+        read-df  (do (g/write-libsvm! libsvm-df temp-file {:mode "overwrite"})
+                     (g/read-libsvm! spark temp-file {"numFeatures" "780"}))]
+    (g/collect read-df) => (g/collect libsvm-df))
+  (let [temp-file (.toString (create-temp-file! ".json"))
+        read-df  (do (g/write-json! write-df temp-file {:mode "overwrite"})
+                     (g/read-json! spark temp-file {}))]
+    (g/collect write-df) => (g/collect read-df)))
+
 (fact "Can read and write csv"
   (let [temp-file (.toString (create-temp-file! ".csv"))
-        read-df  (do (g/write-csv! write-df temp-file)
+        read-df  (do (g/write-csv! write-df temp-file {:mode "overwrite"})
                      (g/read-csv! spark temp-file))]
     (g/collect write-df) => (g/collect read-df)))
 
 (fact "Can read and write parquet"
   (let [temp-file (.toString (create-temp-file! ".parquet"))
-        read-df  (do (g/write-parquet! write-df temp-file)
+        read-df  (do (g/write-parquet! write-df temp-file {:mode "overwrite"})
                      (g/read-parquet! spark temp-file))]
     (g/collect write-df) => (g/collect read-df)))
 
 (fact "Can read and write libsvm"
   (let [temp-file (.toString (create-temp-file! ".libsvm"))
-        read-df  (do (g/write-libsvm! libsvm-df temp-file)
+        read-df  (do (g/write-libsvm! libsvm-df temp-file {:mode "overwrite"})
                      (g/read-libsvm! spark temp-file))]
     (g/collect libsvm-df) => (g/collect read-df)))
 
 (fact "Can read and write json"
   (let [temp-file (.toString (create-temp-file! ".json"))
-        read-df  (do (g/write-json! write-df temp-file)
+        read-df  (do (g/write-json! write-df temp-file {:mode "overwrite"})
                      (g/read-json! spark temp-file))]
     (g/collect write-df) => (g/collect read-df)))
-
-;(fact "Can append json and parquet"
-  ;(let [temp-file (.toString (create-temp-file! ".json"))
-        ;read-df  (do (g/write-json! write-df temp-file {:save-mode :append})
-                     ;(g/write-json! write-df temp-file {:save-mode :append})
-                     ;(g/read-json! spark temp-file))]
-    ;(g/count read-df) => (* 2 (g/count write-df))))
