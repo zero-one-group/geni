050b58bc2620ed68f8b1ed32f69ad95dd8b3e91a
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-15 00:08:38 +0700

    Refactor from sheets (#59)
    * Refactored google sheets module
    
    * tracer bullet for spreadsheet creation

--- examples/sheets.clj
+++ examples/sheets.clj
@@ -1,5 +1,60 @@
-(ns examples.sheets)
+(ns examples.sheets
+  (:require
+    [zero-one.geni.experimental.google-sheets :as gs])
+  (:import
+    (com.google.api.services.sheets.v4 Sheets)
+    (com.google.api.client.googleapis.auth.oauth2 GoogleCredential)
+    (com.google.api.services.sheets.v4 Sheets$Builder
+                                       SheetsScopes)))
 
-;; Setting up credentials:
-;; https://github.com/SparkFund/google-apps-clj#obtaining-oauth-2-credentials
-;; NOTE: enable google sheet API
+
+(import '(com.google.api.services.sheets.v4.model Spreadsheet
+                                                  SpreadsheetProperties));
+
+(def google-props
+  {:credentials    "resources/credentials.json"
+   :spreadsheet-id "1Kit0_YS1RMM1wBL0MksG-0SGuUpni-hbjqKhqL61XYc"})
+
+(def service (gs/sheets-service google-props))
+
+(def placeholder
+  (-> (Spreadsheet.)
+      (.setProperties (SpreadsheetProperties.))))
+
+(def spreadsheet
+  (-> service
+      .spreadsheets
+      (.create placeholder)
+      (.setFields "spreadsheetId")
+      .execute))
+spreadsheet
+
+{"spreadsheetId" "1MO5TjuJPauxhKIM1AsC0JQgRbZ4GEp52qat9iGsYQm8"}
+
+(import '(com.google.api.services.drive Drive$Builder))
+(import '(com.google.api.services.drive.model Permission))
+
+(defn google-credentials [creds-path]
+  (-> (GoogleCredential/fromStream (clojure.java.io/input-stream creds-path))
+      (.createScoped ["https://www.googleapis.com/auth/drive"])))
+
+(def drive-service
+  (-> (Drive$Builder.
+        gs/http-transport
+        gs/json-factory
+        (google-credentials (:credentials google-props)))
+      (.setApplicationName "Geni Lib")
+      .build))
+
+(def new-permission
+  (-> (Permission.)
+      (.setType "user")
+      (.setEmailAddress "anthony@zero-one-group.com")
+      (.setRole "writer")))
+
+(-> drive-service
+    .permissions
+    (.create
+      "1MO5TjuJPauxhKIM1AsC0JQgRbZ4GEp52qat9iGsYQm8"
+      new-permission)
+    .execute)
--- src/zero_one/geni/experimental/google_sheets.clj
+++ src/zero_one/geni/experimental/google_sheets.clj
@@ -13,53 +13,54 @@
 
 (def http-transport (GoogleNetHttpTransport/newTrustedTransport))
 
-(defn google-credentials [creds-path]
+(defn- google-credentials [creds-path]
   (-> (GoogleCredential/fromStream (clojure.java.io/input-stream creds-path))
-      (.createScoped [SheetsScopes/SPREADSHEETS_READONLY])))
+      (.createScoped [SheetsScopes/SPREADSHEETS])))
 
-(defn sheets-service [credentials app-name]
-  (-> (Sheets$Builder. http-transport
-                       json-factory
-                       credentials)
-      (.setApplicationName app-name)
-      .build))
+(defn sheets-service [google-props]
+  (let [app-name       (:app-name google-props "Geni Lib")
+        credentials    (google-credentials (:credentials google-props))]
+    (-> (Sheets$Builder. http-transport json-factory credentials)
+        (.setApplicationName app-name)
+        .build)))
 
-(defn first-sheet-name [service spreadsheet-id]
-  (-> service
-      .spreadsheets
-      (.get spreadsheet-id)
-      .execute
-      .getSheets
-      first
-      .getProperties
-      .getTitle))
+(defn sheet-names [service spreadsheet-id]
+  (let [sheet-objs (-> service
+                       .spreadsheets
+                       (.get spreadsheet-id)
+                       .execute
+                       .getSheets)]
+    (map #(-> % .getProperties .getTitle) sheet-objs)))
 
-(defn first-sheet-values [service spreadsheet-id]
-  (-> service
-      .spreadsheets
-      .values
-      (.get spreadsheet-id (first-sheet-name service spreadsheet-id))
-      .execute
-      .getValues))
+(defn sheet-values [service spreadsheet-id sheet-name]
+  (let [sheet-name (or sheet-name
+                       (first (sheet-names service spreadsheet-id)))
+        value-objs (-> service
+                       .spreadsheets
+                       .values
+                       (.get spreadsheet-id sheet-name)
+                       .execute
+                       .getValues)]
+    (map seq value-objs)))
 
-(defn spreadsheet-values->dataset [spark values]
-  (let [col-names (first values)
-        rows      (rest values)]
+(defn default-columns [n-cols]
+  (->> (range)
+       (map #(keyword (str "_c" %)))
+       (take n-cols)))
+
+(defn spreadsheet-values->dataset [spark values options]
+  (let [header    (:header options true)
+        col-names (if header
+                    (first values)
+                    (default-columns (count (first values))))
+        rows      (if header
+                    (rest values)
+                    values)]
     (g/table->dataset spark rows col-names)))
 
-(defn read-sheets! [spark google-props]
-  (let [app-name       (:app-name google-props "Geni Lib")
-        credentials    (google-credentials (:credentials google-props))
-        spreadsheet-id (:spreadsheet-id google-props)
-        service        (sheets-service credentials app-name)]
-    (spreadsheet-values->dataset spark (first-sheet-values service spreadsheet-id))))
-
-(comment
-  (require '[zero-one.geni.test-resources :refer [spark]])
-
-  (-> (read-sheets! spark {:credentials    "resources/credentials.json"
-                           :spreadsheet-id "1Kit0_YS1RMM1wBL0MksG-0SGuUpni-hbjqKhqL61XYc"})
-      g/show)
-
-  true)
-
+(defn read-sheets! [spark google-props options]
+  (let [service        (sheets-service google-props)
+        values         (sheet-values service
+                                     (:spreadsheet-id google-props)
+                                     (:sheet-name google-props))]
+    (spreadsheet-values->dataset spark values options)))
