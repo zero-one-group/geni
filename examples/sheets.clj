573857bacd6e7747d15718eaee70e47e76520f2f
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-16 13:30:49 +0700

    Tracer bullet for successful write op to sheet

--- examples/sheets.clj
+++ examples/sheets.clj
@@ -1,6 +1,8 @@
 (ns examples.sheets
   (:require
-    [zero-one.geni.experimental.google-sheets :as gs])
+    [zero-one.geni.core :as g]
+    [zero-one.geni.experimental.google-sheets :as gs]
+    [zero-one.geni.test-resources :refer [spark df-20]])
   (:import
     (com.google.api.services.sheets.v4 Sheets)
     (com.google.api.client.googleapis.auth.oauth2 GoogleCredential)
@@ -9,7 +11,17 @@
 
 
 (import '(com.google.api.services.sheets.v4.model Spreadsheet
+                                                  Sheet
+                                                  SheetProperties
                                                   SpreadsheetProperties));
+(import '(com.google.api.services.sheets.v4.model UpdateValuesResponse));
+(import '(com.google.api.services.sheets.v4.model ValueRange));
+
+(def dataframe (-> df-20 (g/select "SellerG" "Date" "Rooms" "Price")))
+(def column-names (g/column-names dataframe))
+(def values (g/collect-vals dataframe))
+(def sheet-name "Melbourne")
+
 
 (def google-props
   {:credentials    "resources/credentials.json"
@@ -17,27 +29,46 @@
 
 (def service (gs/sheets-service google-props))
 
-(def placeholder
+(def target-sheet
+  (-> (Sheet.)
+      (.setProperties (-> (SheetProperties.) (.setTitle sheet-name)))))
+(.getProperties target-sheet)
+
+(def target-spreadsheet
   (-> (Spreadsheet.)
-      (.setProperties (SpreadsheetProperties.))))
+      (.setProperties (SpreadsheetProperties.))
+      (.setSheets [target-sheet])))
 
 (def spreadsheet
   (-> service
       .spreadsheets
-      (.create placeholder)
+      (.create target-spreadsheet)
       (.setFields "spreadsheetId")
       .execute))
 spreadsheet
 
-{"spreadsheetId" "1MO5TjuJPauxhKIM1AsC0JQgRbZ4GEp52qat9iGsYQm8"}
+(def spreadsheet-id "1OtgROqQdXt1XDlLWxXcL_maEEqwBYOntmaxwgrn5gLg")
+(println "Open spreadsheet:")
+(println (str "https://docs.google.com/spreadsheets/d/"
+              spreadsheet-id))
+
+(def body (-> (ValueRange.) (.setValues (conj values column-names))))
+(def sheet-range (str (or sheet-name "Sheet1") "!A1"))
+
+(type service)
+
+(-> service
+    .spreadsheets
+    .values
+    (.update spreadsheet-id sheet-range body)
+    (.setValueInputOption "USER_ENTERED")
+    .execute)
 
 (import '(com.google.api.services.drive Drive$Builder))
 (import '(com.google.api.services.drive.model Permission))
-
 (defn google-credentials [creds-path]
   (-> (GoogleCredential/fromStream (clojure.java.io/input-stream creds-path))
       (.createScoped ["https://www.googleapis.com/auth/drive"])))
-
 (def drive-service
   (-> (Drive$Builder.
         gs/http-transport
@@ -45,16 +76,14 @@ spreadsheet
         (google-credentials (:credentials google-props)))
       (.setApplicationName "Geni Lib")
       .build))
-
 (def new-permission
   (-> (Permission.)
       (.setType "user")
       (.setEmailAddress "anthony@zero-one-group.com")
       (.setRole "writer")))
-
 (-> drive-service
     .permissions
     (.create
-      "1MO5TjuJPauxhKIM1AsC0JQgRbZ4GEp52qat9iGsYQm8"
+      spreadsheet-id
       new-permission)
     .execute)
