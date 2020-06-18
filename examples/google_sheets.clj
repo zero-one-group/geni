eeafdd4bb18157a9a54f6e60150bfffccc950d07
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-19 00:12:41 +0700

    Extracted a with-dynamic-import macro

--- examples/google_sheets.clj
+++ examples/google_sheets.clj
@@ -1,3 +1,15 @@
 (ns examples.xgboost
   (:require
     [zero-one.geni.core :as g]))
+
+(defmacro with-dynamic-import [imports & body]
+  (if (try
+        (eval imports)
+        (catch ClassNotFoundException _ nil))
+    `(do ~@body :succeeded)
+    :failed))
+
+(with-dynamic-import
+  (import '(ml.dmlc.xgboost4j.scala.spark XGBoostClassifier XGBoostRegressor))
+  (defn gg [x] (* x x)))
+(gg 2)
