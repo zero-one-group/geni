b152f599a9cc10a59fdf9c150a413480f095b24f
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-07-28 10:14:55 +0700

    Implicit Spark sessions

--- src/zero_one/geni/data_sources.clj
+++ src/zero_one/geni/data_sources.clj
@@ -1,9 +1,14 @@
 (ns zero-one.geni.data-sources
   (:refer-clojure :exclude [partition-by sort-by])
   (:require
-    [camel-snake-kebab.core :refer [->camelCase]]))
+    [camel-snake-kebab.core :refer [->camelCase]]
+    [zero-one.geni.defaults])
+  (:import
+    (org.apache.spark.sql SparkSession)))
 
 ;; TODO: read-edn!, write-edn!
+
+(def default-spark zero-one.geni.defaults/spark)
 
 (defn configure-reader-or-writer [unconfigured options]
   (reduce
@@ -11,43 +16,71 @@
     unconfigured
     options))
 
-(defn read-data! [format spark path options]
-  (let [unconfigured-reader (.. spark read (format format))
-        configured-reader   (configure-reader-or-writer unconfigured-reader options)]
+(def default-options
+  {"csv" {"header" "true"}})
+
+(defn read-data! [format-name spark path options]
+  (let [defaults            (default-options format-name)
+        unconfigured-reader (.. spark read (format format-name))
+        configured-reader   (configure-reader-or-writer
+                              unconfigured-reader
+                              (merge defaults options))]
     (.load configured-reader path)))
 
-(defn read-avro!
+(defmulti read-avro! (fn [head & _] (class head)))
+(defmethod read-avro! :default
+  ([path] (read-avro! @default-spark path))
+  ([path options] (read-avro! @default-spark path options)))
+(defmethod read-avro! SparkSession
   ([spark path] (read-avro! spark path {}))
   ([spark path options] (read-data! "avro" spark path options)))
 
-(defn read-parquet!
+(defmulti read-parquet! (fn [head & _] (class head)))
+(defmethod read-parquet! :default
+  ([path] (read-parquet! @default-spark path))
+  ([path options] (read-parquet! @default-spark path options)))
+(defmethod read-parquet! SparkSession
   ([spark path] (read-parquet! spark path {}))
   ([spark path options] (read-data! "parquet" spark path options)))
 
-(def default-options
-  {:csv {"header" "true"}})
+(defmulti read-csv! (fn [head & _] (class head)))
+(defmethod read-csv! :default
+  ([path] (read-csv! @default-spark path))
+  ([path options] (read-csv! @default-spark path options)))
+(defmethod read-csv! SparkSession
+  ([spark path] (read-csv! spark path {}))
+  ([spark path options] (read-data! "csv" spark path options)))
 
-(defn read-csv!
-  ([spark path] (read-csv! spark path (:csv default-options)))
-  ([spark path options]
-   (read-data! "csv" spark path (merge (:csv default-options) options))))
-
-(defn read-libsvm!
+(defmulti read-libsvm! (fn [head & _] (class head)))
+(defmethod read-libsvm! :default
+  ([path] (read-libsvm! @default-spark path))
+  ([path options] (read-libsvm! @default-spark path options)))
+(defmethod read-libsvm! SparkSession
   ([spark path] (read-libsvm! spark path {}))
   ([spark path options] (read-data! "libsvm" spark path options)))
 
-(defn read-json!
+(defmulti read-json! (fn [head & _] (class head)))
+(defmethod read-json! :default
+  ([path] (read-json! @default-spark path))
+  ([path options] (read-json! @default-spark path options)))
+(defmethod read-json! SparkSession
   ([spark path] (read-json! spark path {}))
   ([spark path options] (read-data! "json" spark path options)))
 
-(defn read-text!
+(defmulti read-text! (fn [head & _] (class head)))
+(defmethod read-text! :default
+  ([path] (read-text! @default-spark path))
+  ([path options] (read-text! @default-spark path options)))
+(defmethod read-text! SparkSession
   ([spark path] (read-text! spark path {}))
   ([spark path options] (read-data! "text" spark path options)))
 
-(defn read-jdbc! [spark options]
-  (let [unconfigured-reader (.. spark sqlContext read (format "jdbc"))
-        configured-reader   (configure-reader-or-writer unconfigured-reader options)]
-    (.load configured-reader)))
+(defn read-jdbc!
+  ([options] (read-jdbc! @default-spark options))
+  ([spark options]
+   (let [unconfigured-reader (.. spark sqlContext read (format "jdbc"))
+         configured-reader   (configure-reader-or-writer unconfigured-reader options)]
+     (.load configured-reader))))
 
 (defn write-data! [format dataframe path options]
   (let [mode                (:mode options)
--- test/zero_one/geni/data_sources_test.clj
+++ test/zero_one/geni/data_sources_test.clj
@@ -4,15 +4,14 @@
     [zero-one.geni.core :as g]
     [zero-one.geni.test-resources :refer [create-temp-file!
                                           melbourne-df
-                                          libsvm-df
-                                          spark]])
+                                          libsvm-df]])
   (:import
     (org.apache.spark.sql AnalysisException)))
 
 (def write-df
   (-> melbourne-df (g/select :Method :Type) (g/limit 5)))
 
-(fact "Writer defaults to error" :slow
+(fact "Writer defaults to error" ;:slow
   (doall
     (for [write-fn! [g/write-avro!
                      g/write-csv!
@@ -34,73 +33,77 @@
     (g/write-jdbc! write-df (assoc options :mode "overwrite"))
     (g/write-jdbc! write-df options) => (throws AnalysisException)))
 
-(fact "Can read with options" :slow
+(fact "Can read with options" ;:slow
   (let [read-df (g/read-parquet!
-                  spark
                   "test/resources/melbourne_housing_snapshot.parquet"
                   {"mergeSchema" "true"})]
     (g/count read-df) => 13580)
   (let [temp-file (.toString (create-temp-file! ".csv"))
         read-df  (do (g/write-csv! write-df temp-file {:mode "overwrite"})
-                     (g/read-csv! spark temp-file {:header false}))]
+                     (g/read-csv! temp-file {:header false}))]
     (set (g/column-names read-df)) => #(not= % #{:Method :Type}))
   (let [temp-file (.toString (create-temp-file! ".libsvm"))
         read-df  (do (g/write-libsvm! libsvm-df temp-file {:mode "overwrite"})
-                     (g/read-libsvm! spark temp-file {:num-features "780"}))]
+                     (g/read-libsvm! temp-file {:num-features "780"}))]
     (g/collect read-df) => (g/collect libsvm-df))
   (let [temp-file (.toString (create-temp-file! ".json"))
         read-df  (do (g/write-json! write-df temp-file {:mode "overwrite"})
-                     (g/read-json! spark temp-file {}))]
-    (g/collect write-df) => (g/collect read-df)))
+                     (g/read-json! temp-file {}))]
+    (g/collect write-df) => (g/collect read-df))
+  (let [write-df  (g/select write-df :Type)
+        temp-file (.toString (create-temp-file! ".txt"))
+        read-df  (do (g/write-text! write-df temp-file {:mode "overwrite"})
+                     (g/read-text! temp-file {}))]
+    (g/collect-vals write-df) => (g/collect-vals read-df)))
 
 (fact "Can read and write csv"
   (let [temp-file (.toString (create-temp-file! ".csv"))
         read-df  (do (g/write-csv! write-df temp-file {:mode      "overwrite"
                                                        :delimiter "|"})
-                     (g/read-csv! spark temp-file {:delimiter "|"}))]
+                     (g/read-csv! temp-file {:delimiter "|"}))]
     (g/collect write-df) => (g/collect read-df))
   (let [temp-file (.toString (create-temp-file! ".csv"))
         read-df  (do (g/write-csv! write-df temp-file {:mode "overwrite"})
-                     (g/read-csv! spark temp-file))]
+                     (g/read-csv! temp-file))]
     (g/collect write-df) => (g/collect read-df))
   (let [temp-file (.toString (create-temp-file! ".csv"))
         read-df  (do (g/write-csv! write-df temp-file {:mode "overwrite"})
-                     (g/read-csv! spark temp-file {}))]
+                     (g/read-csv! temp-file {}))]
     (g/column-names read-df) => (g/column-names write-df)))
 
 (fact "Can read and write avro"
   (let [temp-file (.toString (create-temp-file! ".avro"))
         read-df  (do (g/write-avro! write-df temp-file {:mode "overwrite"})
-                     (g/read-avro! spark temp-file))]
+                     (g/read-avro! temp-file))]
     (g/collect write-df) => (g/collect read-df))
   (let [temp-file (.toString (create-temp-file! ".avro"))
         read-df  (do (g/write-avro! write-df temp-file {:mode "overwrite"})
-                     (g/read-avro! spark temp-file {}))]
+                     (g/read-avro! temp-file {}))]
     (g/collect write-df) => (g/collect read-df)))
 
 (fact "Can read and write parquet"
   (let [temp-file (.toString (create-temp-file! ".parquet"))
         read-df  (do (g/write-parquet! write-df temp-file {:mode "overwrite"})
-                     (g/read-parquet! spark temp-file))]
+                     (g/read-parquet! temp-file))]
     (g/collect write-df) => (g/collect read-df)))
 
 (fact "Can read and write libsvm"
   (let [temp-file (.toString (create-temp-file! ".libsvm"))
         read-df  (do (g/write-libsvm! libsvm-df temp-file {:mode "overwrite"})
-                     (g/read-libsvm! spark temp-file))]
+                     (g/read-libsvm! temp-file))]
     (g/collect libsvm-df) => (g/collect read-df)))
 
 (fact "Can read and write json"
   (let [temp-file (.toString (create-temp-file! ".json"))
         read-df  (do (g/write-json! write-df temp-file {:mode "overwrite"})
-                     (g/read-json! spark temp-file))]
+                     (g/read-json! temp-file))]
     (g/collect write-df) => (g/collect read-df)))
 
 (fact "Can read and write text"
   (let [write-df  (g/select write-df :Type)
         temp-file (.toString (create-temp-file! ".text"))
         read-df   (do (g/write-text! write-df temp-file {:mode "overwrite"})
-                      (g/read-text! spark temp-file))]
+                      (g/read-text! temp-file))]
     (g/collect-vals write-df) => (g/collect-vals read-df)))
 
 (fact "Can read and write jdbc"
@@ -111,7 +114,7 @@
                                               :driver  "org.sqlite.JDBC"
                                               :url     (str "jdbc:sqlite:" temp-file)
                                               :dbtable "housing"})
-                    (g/read-jdbc! spark {:driver  "org.sqlite.JDBC"
-                                         :url     (str "jdbc:sqlite:" temp-file)
-                                         :dbtable "housing"}))]
+                    (g/read-jdbc! {:driver  "org.sqlite.JDBC"
+                                   :url     (str "jdbc:sqlite:" temp-file)
+                                   :dbtable "housing"}))]
     (g/collect-vals write-df) => (g/collect-vals read-df)))
