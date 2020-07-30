abab60467b144d8465f37ece2e25643b8cde2891
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-07-30 11:20:24 +0700

    version bump to 0.0.20 (#129)

--- docker/project.clj
+++ docker/project.clj
@@ -1,6 +1,8 @@
 (def spark-deps
   '[;; Spark
-    [com.github.fommil.netlib/all "1.1.2" :extension "pom"]
+    ; This breaks cljcdoc: https://github.com/cljdoc/cljdoc/issues/407
+    ; Frozen until issue is resolved.
+    ;[com.github.fommil.netlib/all "1.1.2" :extension "pom"]
     [org.apache.spark/spark-avro_2.12 "3.0.0"]
     [org.apache.spark/spark-core_2.12 "3.0.0"]
     [org.apache.spark/spark-hive_2.12 "3.0.0"]
@@ -15,7 +17,7 @@
     [ml.dmlc/xgboost4j-spark_2.12 "1.0.0"]
     [ml.dmlc/xgboost4j_2.12 "1.0.0"]])
 
-(defproject zero.one/geni "0.0.19"
+(defproject zero.one/geni "0.0.20"
   :jvm-opts ["-Duser.country=US" "-Duser.language=en"]
   :description "A Clojure library that wraps Apache Spark"
   :url "https://github.com/zero-one-group/geni"
