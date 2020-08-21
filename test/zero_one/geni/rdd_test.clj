42919381e1f17aa6eed02d12b4af22a10b480851
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-08-21 14:55:13 +0700

    Added more coverage

--- test/zero_one/geni/rdd_test.clj
+++ test/zero_one/geni/rdd_test.clj
@@ -37,7 +37,10 @@
   (let [result (rdd/count-approx dummy-rdd 1000)]
     (rdd/initial-value result) => #(every? % [:mean :low :high :confidence])
     (rdd/final-value result) => #(every? % [:mean :low :high :confidence])
-    (rdd/final? result) => boolean?))
+    (rdd/final? result) => boolean?)
+  (-> (rdd/count-approx dummy-rdd 1000 0.9)
+      rdd/initial-value
+      :low) => #(< 100 %))
 
 (facts "On basic RDD actions" :rdd
   (fact "collect-async works"
