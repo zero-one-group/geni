829aa4771dd44ca232352b646df433312edc25a4
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-09-17 08:02:02 +0700

    Missed dep in fxl

--- lein-template/resources/leiningen/new/geni/project.clj
+++ lein-template/resources/leiningen/new/geni/project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [zero.one/geni "0.0.27"]
+                 [expound "0.8.5"]
                  ;; Spark
                  [org.apache.spark/spark-core_2.12 "3.0.1"]
                  [org.apache.spark/spark-hive_2.12 "3.0.1"]
