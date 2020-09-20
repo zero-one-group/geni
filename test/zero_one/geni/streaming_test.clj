0966db4034239e6dd9892aa10ae28ce6165bd089
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-09-20 11:19:29 +0700

    Close to a d-stream testing env

--- test/zero_one/geni/streaming_test.clj
+++ test/zero_one/geni/streaming_test.clj
@@ -1,18 +1,55 @@
 (ns zero-one.geni.streaming-test
   (:require
+    [clojure.java.io :as io]
     [midje.sweet :refer [facts fact =>]]
+    [zero-one.geni.spark :as spark]
     [zero-one.geni.defaults :as defaults]
-    [zero-one.geni.streaming :as stream])
+    [zero-one.geni.streaming :as streaming]
+    [zero-one.geni.test-resources :refer [create-temp-file!]])
   (:import
     (org.apache.spark.streaming Duration StreamingContext)))
+
+
+(defn create-random-file! [file-name]
+  (let [file (io/file (str "target/" (java.util.UUID/randomUUID) "/" file-name))]
+    (io/make-parents file)
+    file))
+
+(defn stream-results [opts]
+  (let [new-context (streaming/streaming-context
+                      (spark/create-spark-session {})
+                      (:duration opts (streaming/milliseconds 100)))
+        read-file  (create-random-file! "/read.txt")
+        write-file (create-random-file! "/write")
+        d-stream   (streaming/text-file-stream
+                     new-context
+                     (-> read-file .getParent .toString))]
+    (spit read-file "=== START ===\n")
+    (streaming/save-as-text-files! d-stream (-> write-file .toString))
+    (streaming/start! new-context)
+    ;; TODO: must wait until tihs starts
+    (doall
+      (for [line (:lines opts)]
+        (do
+          (Thread/sleep 200)
+          (spit read-file (str line "\n") :append true))))
+    (spit read-file "=== STOP ===\n" :append true)
+    (streaming/await-termination! new-context)
+    @(streaming/stop! new-context)
+    (print write-file)
+    ;; TODO: grab all filse that has part in it
+    (-> write-file .getParent io/file file-seq)))
+
+(facts "On DStream testing" :streaming
+  (stream-results {:lines (range 10)}) => 1)
 
 (facts "On durations" :streaming
   (fact "durations instantiatable"
     (mapv
       (fn [f] (f 1) => (partial instance? Duration))
-      [stream/milliseconds stream/seconds stream/minutes])))
+      [streaming/milliseconds streaming/seconds streaming/minutes])))
 
 (facts "On StreamingContext" :streaming
   (fact "streaming context instantiatable"
-    (stream/streaming-context @defaults/spark (stream/seconds 1))
+    (streaming/streaming-context @defaults/spark (streaming/seconds 1))
     => (partial instance? StreamingContext)))
