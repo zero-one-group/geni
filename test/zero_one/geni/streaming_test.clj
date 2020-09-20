7362599b47f8d5445e47e5d94c61a3db6060ad51
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-09-20 13:36:52 +0700

    Usable d-stream testing function

--- test/zero_one/geni/streaming_test.clj
+++ test/zero_one/geni/streaming_test.clj
@@ -1,47 +1,48 @@
 (ns zero-one.geni.streaming-test
   (:require
+    [clojure.string :as string]
     [clojure.java.io :as io]
     [midje.sweet :refer [facts fact =>]]
     [zero-one.geni.spark :as spark]
     [zero-one.geni.defaults :as defaults]
-    [zero-one.geni.streaming :as streaming]
-    [zero-one.geni.test-resources :refer [create-temp-file!]])
+    [zero-one.geni.streaming :as streaming])
   (:import
     (org.apache.spark.streaming Duration StreamingContext)))
-
 
 (defn create-random-file! [file-name]
   (let [file (io/file (str "target/" (java.util.UUID/randomUUID) "/" file-name))]
     (io/make-parents file)
     file))
 
+(defn written-content [write-file]
+  (let [files         (-> write-file .getParent io/file file-seq)
+        slurped-texts (->> files
+                           (filter #(.isFile %))
+                           (map #(.toString %))
+                           (filter #(string/includes? % "/part"))
+                           (map slurp))]
+    (string/join slurped-texts)))
+
 (defn stream-results [opts]
-  (let [new-context (streaming/streaming-context
-                      (spark/create-spark-session {})
-                      (:duration opts (streaming/milliseconds 100)))
-        read-file  (create-random-file! "/read.txt")
-        write-file (create-random-file! "/write")
-        d-stream   (streaming/text-file-stream
-                     new-context
-                     (-> read-file .getParent .toString))]
-    (spit read-file "=== START ===\n")
-    (streaming/save-as-text-files! d-stream (-> write-file .toString))
-    (streaming/start! new-context)
-    ;; TODO: must wait until tihs starts
-    (doall
-      (for [line (:lines opts)]
-        (do
-          (Thread/sleep 200)
-          (spit read-file (str line "\n") :append true))))
-    (spit read-file "=== STOP ===\n" :append true)
-    (streaming/await-termination! new-context)
-    @(streaming/stop! new-context)
-    (print write-file)
-    ;; TODO: grab all filse that has part in it
-    (-> write-file .getParent io/file file-seq)))
+  (let [context         (streaming/streaming-context
+                          (spark/create-spark-session {})
+                          (:duration opts (streaming/milliseconds 100)))
+        read-file      (create-random-file! "read.txt")
+        write-file     (create-random-file! "write")
+        input-stream   (streaming/text-file-stream
+                         context
+                         (-> read-file .getParent .toString))
+        d-stream        ((:d-stream-fn opts identity) input-stream)]
+    (spit read-file "")
+    (streaming/save-as-text-files! d-stream (.toString write-file))
+    @(streaming/start! context)
+    (spit read-file (str (:content opts "Hello World!")))
+    (streaming/await-termination! context)
+    @(streaming/stop! context)
+    (written-content write-file)))
 
 (facts "On DStream testing" :streaming
-  (stream-results {:lines (range 10)}) => 1)
+  (stream-results {:content (range 10)}) => (str (range 10) "\n"))
 
 (facts "On durations" :streaming
   (fact "durations instantiatable"
