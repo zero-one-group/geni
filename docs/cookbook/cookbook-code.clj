66f785238284605878dc44881e80dc57ae0c4c3f
Burin, Choomnuan <agilecreativity@gmail.com>
2020-10-08 00:00:42 -0400

    Remove cookbook-01 code from original file

--- docs/cookbook/cookbook-code.clj
+++ docs/cookbook/cookbook-code.clj
@@ -13,66 +13,6 @@
       (clojure.java.shell/sh "wget" "-O" target-path source-url)
       :downloaded)))
 
-;; Part 1: Reading and Writing Datasets
-(def bikes-data-url "https://raw.githubusercontent.com/jvns/pandas-cookbook/master/data/bikes.csv")
-(def bikes-data-path "data/cookbook/bikes.csv")
-(download-data! bikes-data-url bikes-data-path)
-
-;; 1.1 Reading Data from a CSV File
-(def broken-df (g/read-csv! bikes-data-path))
-(-> broken-df
-    (g/limit 3)
-    g/show)
-
-(def fixed-df
-  (g/read-csv! bikes-data-path {:delimiter ";" :encoding "ISO-8859-1"}))
-
-(-> fixed-df
-    (g/limit 3)
-    g/show)
-
-(-> fixed-df (g/limit 3) g/show-vertical)
-
-(g/count fixed-df)
-
-(g/print-schema fixed-df)
-
-(-> fixed-df (g/limit 3) g/collect)
-
-;; 1.2 Selecting and Renaming Columns
-(-> fixed-df
-    (g/select :Date "Berri 1")
-    (g/limit 3)
-    g/show)
-
-(-> fixed-df
-    (g/select {:date "Date" :berri-1 "Berri 1"})
-    (g/limit 3)
-    g/show)
-
-(def renamed-df
-  (-> fixed-df
-      (g/to-df :date
-               :berri-1
-               :brebeuf
-               :cote-sainte-catherine
-               :maisonneuve-1
-               :maisonneuve-2
-               :du-parc
-               :pierre-dupuy
-               :rachel-1
-               :st-urbain)))
-
-(-> renamed-df (g/limit 3) g/show)
-
-;; 1.3 Describing Columns
-(-> renamed-df
-    g/describe
-    g/show)
-
-;; 1.4 Writing Datasets
-(g/write-parquet! renamed-df "data/cookbook/bikes.parquet" {:mode "overwrite"})
-
 ;; Part 2: Selecting Rows and Columns
 (def complaints-data-url
   "https://raw.githubusercontent.com/jvns/pandas-cookbook/master/data/311-service-requests.csv")
