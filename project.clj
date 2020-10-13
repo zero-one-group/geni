f3b6e577b808440f962b01150455d0a517c7a13e
anthony-khong <anthony.kusumo.khong@gmail.com>
2020-10-13 16:45:24 +0700

    Upgraded fxl version

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
                  [org.clojure/java.data "1.0.86"]
                  [potemkin "0.4.5"]
                  [reply "0.4.4" :exclusions [javax.servlet/servlet-api]]
-                 [zero.one/fxl "0.0.4"]]
+                 [zero.one/fxl "0.0.5"]]
   :profiles
   {:provided {:dependencies ~spark-deps}
    :uberjar {:aot :all :dependencies ~spark-deps}
