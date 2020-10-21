2254aeca20e72f3b1e21cd3c79dbc6934106ac5b
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-10-21 08:18:24 +0700

    Version bump v0.0.33 (#274)
    * version bump to v0.0.33
    
    * Make docs test less stringent

--- lein-template/resources/leiningen/new/geni/project.clj
+++ lein-template/resources/leiningen/new/geni/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [zero.one/geni "0.0.32"]
+                 [zero.one/geni "0.0.33"]
                  [expound "0.8.6"]
                  ;; Spark
                  [org.apache.spark/spark-core_2.12 "3.0.1"]
