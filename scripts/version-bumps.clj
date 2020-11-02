c787cf0f810481b3c124aaeec8dbbf44c235383b
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-11-02 17:10:34 +0700

    First draft of handling deps.edn version bump

--- scripts/version-bumps.clj
+++ scripts/version-bumps.clj
@@ -15,6 +15,12 @@
 (def generated-project-clj-path
   "lein-template/resources/leiningen/new/geni/project.clj")
 
+(def docker-deps-edn-path
+  "docker/deps.edn")
+
+(def example-clj-app-deps-edn-path
+  "examples/geni-clj-app/deps.edn")
+
 ;; Bump GENI_RELEASED_VERSION
 (def current-released-version
   (string/replace (slurp released-version-path) #"\n" ""))
@@ -56,10 +62,28 @@
                   (re-pattern (dep-str project-name current-released-version))
                   (dep-str project-name bumped-released-version)))
 
-;; FIXME: add another subsitution for examples/geni-clj-app/deps.edn
+;; deps.edn
+(defn mvn-str [version]
+  (str ":mvn/version \"" version "\""))
+
+(def current-docker-deps-edn (slurp docker-deps-edn-path))
+
+(def bumped-docker-deps-edn
+  (string/replace current-docker-deps-edn
+                  (re-pattern (mvn-str current-released-version))
+                  (mvn-str bumped-released-version)))
+
+(def current-example-clj-app-deps-edn (slurp example-clj-app-deps-edn-path))
+
+(def bumped-example-clj-app-deps-edn
+  (string/replace current-example-clj-app-deps-edn
+                  (re-pattern (mvn-str current-released-version))
+                  (mvn-str bumped-released-version)))
 
 ;; Write the changes
 (spit project-clj-path bumped-project-clj)
 (spit template-project-clj-path bumped-template-project-clj)
 (spit generated-project-clj-path bumped-generated-project-clj)
 (spit released-version-path bumped-released-version)
+(spit docker-deps-edn-path bumped-docker-deps-edn)
+(spit example-clj-app-deps-edn-path bumped-example-clj-app-deps-edn)
