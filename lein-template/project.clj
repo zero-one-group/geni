d89289a9db71d47f36ca27e46066550cd0b352b3
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-11-11 20:12:02 +0700

    Version bump v0.0.35 (#291)
    * version bump to v0.0.35
    
    * Disabled deps.edn syncing during build

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,4 +1,4 @@
-(defproject geni/lein-template "0.0.34"
+(defproject geni/lein-template "0.0.35"
   :description "Leiningen template for a Geni application."
   :url "https://github.com/zero-one-group/geni/tree/develop/lein-template"
   :license {:name "Apache License"
--- lein-template/resources/leiningen/new/geni/project.clj
+++ lein-template/resources/leiningen/new/geni/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [zero.one/geni "0.0.34"]
+                 [zero.one/geni "0.0.35"]
                  [metosin/jsonista "0.2.7"]
                  [expound "0.8.6"]
                  ;; Spark
