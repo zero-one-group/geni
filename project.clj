a2fdbe550518e1e0b6e5524a870e4ef2ce88a42c
skylee03 <1178715749@qq.com>
2023-11-22 20:44:24 +0800

    Update Parsley to 0.9.3

--- project.clj
+++ project.clj
@@ -38,10 +38,11 @@
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[camel-snake-kebab "0.4.2"]
+                 [com.taoensso/nippy "3.3.0"]
                  [expound "0.8.9"]
                  [metosin/jsonista "0.3.3"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
-                 [com.taoensso/nippy "3.3.0"]
+                 [net.cgrand/parsley "0.9.3" :exclusions [org.clojure/clojure]]
                  [nrepl "0.8.3"]
                  [org.clojure/clojure "1.10.3"]
                  [org.clojure/java.data "1.0.86"]
